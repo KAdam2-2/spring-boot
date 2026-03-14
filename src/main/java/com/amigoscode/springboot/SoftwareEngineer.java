3b9fead32588b3cf86bfa90bac081225ee9ae9d8
KAdam2-2 <keruadam01@gmail.com>
2026-03-14 23:13:33 +0300

    mapped java class to database table

--- src/main/java/com/amigoscode/springboot/SoftwareEngineer.java
+++ src/main/java/com/amigoscode/springboot/SoftwareEngineer.java
@@ -1,9 +1,16 @@
 package com.amigoscode.springboot;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity
 public class SoftwareEngineer {
+
+    @Id
     private Integer id;
+
     private String name;
     private String techStack;
 
