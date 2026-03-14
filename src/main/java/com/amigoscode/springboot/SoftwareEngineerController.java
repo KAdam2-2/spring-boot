91cded22ed494b23ec31f16ecc36cc132973edf5
KAdam2-2 <keruadam01@gmail.com>
2026-03-15 00:42:08 +0300

    added getEngineersByiD

--- src/main/java/com/amigoscode/springboot/SoftwareEngineerController.java
+++ src/main/java/com/amigoscode/springboot/SoftwareEngineerController.java
@@ -1,8 +1,6 @@
 package com.amigoscode.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +15,20 @@ public class SoftwareEngineerController {
     }
 
     @GetMapping
-    public List<SoftwareEngineer> getAllSoftwareEngineers(){
+    public List<SoftwareEngineer> getEngineers(){
         return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineersById(
+            @PathVariable("id") Integer Id
+    ){
+        return softwareEngineerService.getSoftwareEngineersById(Id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+        softwareEngineerService.insertsoftwareEngineer(softwareEngineer);
     }
 
 }
