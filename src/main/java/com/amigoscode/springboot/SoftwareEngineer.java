884fd66d6d36512b665a61d2392ad2891a864f27
KAdam2-2 <keruadam01@gmail.com>
2026-03-16 12:17:23 +0300

    Added lombok, @Setter, @Getter, @NoArgsConstructor and @AllArgsConstructor annotations

--- src/main/java/com/amigoscode/springboot/SoftwareEngineer.java
+++ src/main/java/com/amigoscode/springboot/SoftwareEngineer.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
+@Setter
+@Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class SoftwareEngineer {
 
     @Id
@@ -15,41 +23,6 @@ public class SoftwareEngineer {
     private Integer id;
     private String name;
     private String techStack;
-
-    public SoftwareEngineer() {
-    }
-
-    public SoftwareEngineer(Integer id,
-                            String name,
-                            String techStack) {
-        this.id = id;
-        this.name = name;
-        this.techStack = techStack;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTechStack() {
-        return techStack;
-    }
-
-    public void setTechStack(String techStack) {
-        this.techStack = techStack;
-    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/com/amigoscode/springboot/SoftwareEngineerRepository.java
+++ src/main/java/com/amigoscode/springboot/SoftwareEngineerRepository.java
@@ -1,7 +1,9 @@
 package com.amigoscode.springboot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer>{
 
 
